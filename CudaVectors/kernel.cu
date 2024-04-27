f84a7526905b778cbb7a8546c319a56a697ec616
odesai840 <odesai840@gmail.com>
2024-04-27 01:46:16 -0400

    Add project files.

---
+++ CudaVectors/kernel.cu
@@ -0,0 +1,37 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void addVectors(int* a, int* b, int* c) {
+    int i = threadIdx.x;
+
+    c[i] = a[i] + b[i];
+}
+
+int main()
+{
+    int a[] = { 1,2,3 };
+    int b[] = { 4,5,6 };
+    int c[sizeof(a) / sizeof(int)] = { 0 };
+
+    int* cudaA = 0;
+    int* cudaB = 0;
+    int* cudaC = 0;
+
+    // allocate gpu memory
+    cudaMalloc(&cudaA, sizeof(a));
+    cudaMalloc(&cudaB, sizeof(b));
+    cudaMalloc(&cudaC, sizeof(c));
+
+    // copy vectors to gpu memory
+    cudaMemcpy(cudaA, a, sizeof(a), cudaMemcpyHostToDevice);
+    cudaMemcpy(cudaB, b, sizeof(b), cudaMemcpyHostToDevice);
+
+    // run addVectors function with grid of 1 block and pass parameters
+    addVectors <<< 1, sizeof(a) / sizeof(int) >>> (cudaA, cudaB, cudaC);
+
+    cudaMemcpy(c, cudaC, sizeof(c), cudaMemcpyDeviceToHost);
+
+    return 0;
+}
